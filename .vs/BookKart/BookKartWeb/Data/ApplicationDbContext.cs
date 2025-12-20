6c5d82510120547c3a594e2dfc71d03decf3a1ae
Shendge18 <82372567+Shendge18@users.noreply.github.com>
2025-12-21 02:58:10 +0530

    Added Category Page and functions

---
+++ .vs/BookKart/BookKartWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using BookKartWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookKartWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<CategoryDALModel> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CategoryDALModel>().HasData(
+                new CategoryDALModel { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new CategoryDALModel { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new CategoryDALModel { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
+    }
+}
---
+++ .vs/BookKart/BookKartWeb/Models/CategoryDALModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookKartWeb.Models
+{
+    public class CategoryDALModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100)]
+        public int DisplayOrder { get; set; }
+    }
+}
