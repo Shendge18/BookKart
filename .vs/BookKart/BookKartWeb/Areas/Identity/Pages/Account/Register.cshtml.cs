f9655f634cfd20e5c66bd87b8108fc21fd549570
Shendge18 <82372567+Shendge18@users.noreply.github.com>
2026-01-05 00:45:17 +0530

    Added Authorization for creating Admin and Employee Accounts

--- .vs/BookKart/BookKartWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ .vs/BookKart/BookKartWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -211,7 +211,14 @@ namespace BookKartWeb.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        if (User.IsInRole(SD.Role_Admin))
+                        {
+                            TempData["successMsg"] = "New User Created Successfully";
+                        }
+                        else
+                        {
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+                        }
                         return LocalRedirect(returnUrl);
                     }
                 }
