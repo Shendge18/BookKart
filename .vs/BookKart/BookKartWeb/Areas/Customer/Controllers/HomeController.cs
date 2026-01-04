6f92a48be044e3bca72d9d2d0a12e06ee521a780
Shendge18 <82372567+Shendge18@users.noreply.github.com>
2026-01-04 23:23:49 +0530

    Added View Component for Cart

--- .vs/BookKart/BookKartWeb/Areas/Customer/Controllers/HomeController.cs
+++ .vs/BookKart/BookKartWeb/Areas/Customer/Controllers/HomeController.cs
@@ -22,14 +22,6 @@ namespace BookKartWeb.Areas.Customer.Controllers
 
         public IActionResult Index()
         {
-            var claimsIdentity = (ClaimsIdentity)User.Identity;
-            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
-
-            if (claim != null)
-            {
-                HttpContext.Session.SetInt32(SD.SessionCart,
-                _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value).Count());
-            }
             IEnumerable<ProductDALModel> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
             return View(productList);
         }
